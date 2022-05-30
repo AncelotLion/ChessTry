33648e6e1b7e2696a3dd8ff14dd600964c7c1346
AncelotLion <ancelot.lion@gmail.com>
2022-05-30 18:55:20 +0300

    Icons OK

--- src/Button.java
+++ src/Button.java
@@ -4,8 +4,11 @@ import java.awt.event.ActionListener;
 import javax.swing.*;
 
 public class Button extends JFrame {
-    JButton udaff = new JButton("Tura");// название кнопки
-    JButton conHorse = new JButton("Horse");
+    Icon iconTura = new ImageIcon("src/tura.png");
+    JButton buttonTura = new JButton(iconTura);
+    Icon iconHorse = new ImageIcon("src/horse.png");
+    JButton conHorse = new JButton(iconHorse);
+
 
     public Button() {
         super("Chess");
@@ -14,14 +17,16 @@ public class Button extends JFrame {
 
         Container container = this.getContentPane();
         container.setLayout(null);
-        container.add(udaff);
-        udaff.setBounds(0,0,75,75);
 
         container.add(conHorse);
         conHorse.setBounds(76, 0, 75, 75);
 
+        container.add(buttonTura);
+        buttonTura.setBounds(0,0,75,75);
 
-        udaff.addActionListener(new ActionListener() {
+
+
+        buttonTura.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
 
             }
