9adcf95fa79f2c2f21403a6c50f9cb8440cbdcf1
AncelotLion <ancelot.lion@gmail.com>
2022-05-30 22:28:26 +0300

    piesces try

--- src/Button.java
+++ src/Button.java
@@ -3,36 +3,55 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.*;
 
-public class Button extends JFrame {
+public class Button extends JFrame implements ActionListener {
     Icon iconTura = new ImageIcon("src/tura.png");
     JButton buttonTura = new JButton(iconTura);
+
     Icon iconHorse = new ImageIcon("src/horse.png");
     JButton conHorse = new JButton(iconHorse);
 
+    Icon iconPiece = new ImageIcon("src/piece.png");
+    JButton piece = new JButton(iconPiece);
+
+
 
     public Button() {
-        super("Chess");
-        this.setBounds(100,100,720,720);
+        super("ChessLMAO");
+        this.setBounds(100, 100, 720, 720);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container container = this.getContentPane();
         container.setLayout(null);
 
+        container.add(buttonTura);
+        buttonTura.setBounds(0, 0, 75, 75);
+        buttonTura.addActionListener(this);
+
         container.add(conHorse);
         conHorse.setBounds(76, 0, 75, 75);
-
-        container.add(buttonTura);
-        buttonTura.setBounds(0,0,75,75);
+        conHorse.addActionListener(this);
 
 
 
-        buttonTura.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
+        }
 
+
+        /*for (int i = 0; i < 8; i++) {
+                container.add(piece);
+            for (int j = 0; j < 725; j++) {
+                piece.setBounds(j, 75, 75, 75);
+                j += 75;
             }
-        });
+                piece.addActionListener(this);
+            }
 
-    }
+        }*/
+
+        /*container.add(piece);
+        piece.setBounds(0, 75, 75, 75);
+        piece.addActionListener(this);*/
+
+
 
 
 
@@ -40,10 +59,13 @@ public class Button extends JFrame {
         Button app = new Button();
         app.setVisible(true);
 
-
-
-
-
-
     }
-}
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+
+
